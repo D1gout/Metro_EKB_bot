6d23003e19e288e36d1510f83a25a5b9273a880c
D1gout <fanolkon@gmail.com>
2023-06-21 16:11:22 +0500

    99% complete

--- Bot.py
+++ Bot.py
@@ -1,12 +1,13 @@
 import asyncio
 import logging
 import sqlite3
+import time
 from datetime import datetime
 import CheckTime
 from TimeList import TimeList
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils.exceptions import BotBlocked
+from aiogram.utils.exceptions import BotBlocked, MessageNotModified
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -33,6 +34,8 @@ link_button = InlineKeyboardMarkup().add(InlineKeyboardButton(
     'Написать мне',
     url='https://t.me/Aweyout'))
 
+now_time = time.strftime("%H:%M")
+
 st1 = InlineKeyboardButton('Ботаническая (1)', callback_data='st1_click')
 st2 = InlineKeyboardButton('Чкаловская (2)', callback_data='st2_click')
 st3 = InlineKeyboardButton('Геологическая (3)', callback_data='st3_click')
@@ -42,6 +45,26 @@ st6 = InlineKeyboardButton('Уральская (6)', callback_data='st6_click')
 st7 = InlineKeyboardButton('Машиностроителей (7)', callback_data='st7_click')
 st8 = InlineKeyboardButton('Уралмаш (8)', callback_data='st8_click')
 st9 = InlineKeyboardButton('Проспект космонавтов (9)', callback_data='st9_click')
+
+st1_update = InlineKeyboardButton('Ботаническая (1)', callback_data='st1_update')
+st2_update = InlineKeyboardButton('Чкаловская (2)', callback_data='st2_update')
+st3_update = InlineKeyboardButton('Геологическая (3)', callback_data='st3_update')
+st4_update = InlineKeyboardButton('Площадь 1905 года (4)', callback_data='st4_update')
+st5_update = InlineKeyboardButton('Динамо (5)', callback_data='st5_update')
+st6_update = InlineKeyboardButton('Уральская (6)', callback_data='st6_update')
+st7_update = InlineKeyboardButton('Машиностроителей (7)', callback_data='st7_update')
+st8_update = InlineKeyboardButton('Уралмаш (8)', callback_data='st8_update')
+st9_update = InlineKeyboardButton('Проспект космонавтов (9)', callback_data='st9_update')
+
+st1_button = InlineKeyboardMarkup().add(st1_update)
+st2_button = InlineKeyboardMarkup().add(st2_update)
+st3_button = InlineKeyboardMarkup().add(st3_update)
+st4_button = InlineKeyboardMarkup().add(st4_update)
+st5_button = InlineKeyboardMarkup().add(st5_update)
+st6_button = InlineKeyboardMarkup().add(st6_update)
+st7_button = InlineKeyboardMarkup().add(st7_update)
+st8_button = InlineKeyboardMarkup().add(st8_update)
+st9_button = InlineKeyboardMarkup().add(st9_update)
 
 station_buttons = InlineKeyboardMarkup() \
     .add(st1).add(st2).add(st3).add(st4).add(st5).add(st6).add(st7).add(st8).add(st9)
@@ -53,8 +76,22 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     try:
         await bot.send_message(callback_query.from_user.id, f"В сторону {st2.text}\n\n"
                                                             f"{CheckTime.CheckTime(TimeList.st1_B)}",
-                               reply_markup=button_restart)
+                               reply_markup=st1_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st1_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st2.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st1_B)}",
+                                    reply_markup=st1_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -66,8 +103,24 @@ async def process_callback_button(callback_query: types.CallbackQuery):
                                                             f"{CheckTime.CheckTime(TimeList.st2_in_B)}\n\n\n"
                                                             f"В сторону {st3.text}\n\n"
                                                             f"{CheckTime.CheckTime(TimeList.st2_in_P)}",
-                               reply_markup=button_restart)
+                               reply_markup=st2_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st2_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st1.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st2_in_B)}\n\n\n"
+                                         f"В сторону {st3.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st2_in_P)}",
+                                    reply_markup=st2_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -75,9 +128,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st2.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st3_in_B)}\n\n\n"
+                                                            f"В сторону {st4.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st3_in_P)}",
+                               reply_markup=st3_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st3_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st2.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st3_in_B)}\n\n\n"
+                                         f"В сторону {st4.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st3_in_P)}",
+                                    reply_markup=st3_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -85,9 +157,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st3.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st4_in_B)}\n\n\n"
+                                                            f"В сторону {st5.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st4_in_P)}",
+                               reply_markup=st4_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st4_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st3.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st4_in_B)}\n\n\n"
+                                         f"В сторону {st5.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st4_in_P)}",
+                                    reply_markup=st4_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -95,9 +186,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st4.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st5_in_B)}\n\n\n"
+                                                            f"В сторону {st6.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st5_in_P)}",
+                               reply_markup=st5_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st5_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st4.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st5_in_B)}\n\n\n"
+                                         f"В сторону {st6.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st5_in_P)}",
+                                    reply_markup=st5_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -105,9 +215,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st5.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st6_in_B)}\n\n\n"
+                                                            f"В сторону {st7.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st6_in_P)}",
+                               reply_markup=st6_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st6_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st5.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st6_in_B)}\n\n\n"
+                                         f"В сторону {st7.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st6_in_P)}",
+                                    reply_markup=st6_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -115,9 +244,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st6.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st7_in_B)}\n\n\n"
+                                                            f"В сторону {st8.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st7_in_P)}",
+                               reply_markup=st7_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st7_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st6.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st7_in_B)}\n\n\n"
+                                         f"В сторону {st8.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st7_in_P)}",
+                                    reply_markup=st7_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -125,9 +273,28 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st7.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st8_in_B)}\n\n\n"
+                                                            f"В сторону {st9.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st8_in_P)}",
+                               reply_markup=st8_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st8_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st7.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st8_in_B)}\n\n\n"
+                                         f"В сторону {st9.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st8_in_P)}",
+                                    reply_markup=st8_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
@@ -135,9 +302,24 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     try:
-        await bot.send_message(callback_query.from_user.id, 'Ваше направление',
-                               reply_markup=button_restart)
+        await bot.send_message(callback_query.from_user.id, f"В сторону {st8.text}\n\n"
+                                                            f"{CheckTime.CheckTime(TimeList.st9_P)}",
+                               reply_markup=st9_button)
     except BotBlocked:
+        await asyncio.sleep(0.1)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'st9_update')
+async def process_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+
+    try:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=f"Обновлено в {now_time}\n\n"
+                                         f"В сторону {st8.text}\n\n"
+                                         f"{CheckTime.CheckTime(TimeList.st9_P)}",
+                                    reply_markup=st9_button)
+    except MessageNotModified:
         await asyncio.sleep(0.1)
 
 
