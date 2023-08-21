03b07da8639d1a645e3778caf26207cfa21ec1dc
D1gout <fanolkon@gmail.com>
2023-08-22 01:36:41 +0500

    Update code

--- TimeList.py
+++ TimeList.py
@@ -5,66 +5,66 @@ from parse import Parse_Time
 
 class TimeList:
     if datetime.today().isoweekday() != 6 or 7:
-        st1_B = Parse_Time(9)[0].text.replace(' ', '').replace(',', '').split(';')
+        st1_B = Parse_Time(9, 0)
 
-        st2_in_B = Parse_Time(8)[0].text.replace(' ', '').replace(',', '').split(';')
+        st2_in_B = Parse_Time(8, 0)
 
-        st2_in_P = Parse_Time(8)[1].text.replace(' ', '').replace(',', '').split(';')
+        st2_in_P = Parse_Time(8, 1)
 
-        st3_in_B = Parse_Time(7)[0].text.replace(' ', '').replace(',', '').split(';')
+        st3_in_B = Parse_Time(7, 0)
 
-        st3_in_P = Parse_Time(7)[1].text.replace(' ', '').replace(',', '').split(';')
+        st3_in_P = Parse_Time(7, 1)
 
-        st4_in_B = Parse_Time(6)[0].text.replace(' ', '').replace(',', '').split(';')
+        st4_in_B = Parse_Time(6, 0)
 
-        st4_in_P = Parse_Time(6)[1].text.replace(' ', '').replace(',', '').split(';')
+        st4_in_P = Parse_Time(6, 1)
 
-        st5_in_B = Parse_Time(5)[0].text.replace(' ', '').replace(',', '').split(';')
+        st5_in_B = Parse_Time(5, 0)
 
-        st5_in_P = Parse_Time(5)[1].text.replace(' ', '').replace(',', '').split(';')
+        st5_in_P = Parse_Time(5, 1)
 
-        st6_in_B = Parse_Time(4)[0].text.replace(' ', '').replace(',', '').split(';')
+        st6_in_B = Parse_Time(4, 0)
 
-        st6_in_P = Parse_Time(4)[1].text.replace(' ', '').replace(',', '').split(';')
+        st6_in_P = Parse_Time(4, 1)
 
-        st7_in_B = Parse_Time(3)[0].text.replace(' ', '').replace(',', '').split(';')
+        st7_in_B = Parse_Time(3, 0)
 
-        st7_in_P = Parse_Time(3)[1].text.replace(' ', '').replace(',', '').split(';')
+        st7_in_P = Parse_Time(3, 1)
 
-        st8_in_B = Parse_Time(2)[0].text.replace(' ', '').replace(',', '').split(';')
+        st8_in_B = Parse_Time(2, 0)
 
-        st8_in_P = Parse_Time(2)[1].text.replace(' ', '').replace(',', '').split(';')
+        st8_in_P = Parse_Time(2, 1)
 
-        st9_P = Parse_Time(1)[0].text.replace(' ', '').replace(',', '').split(';')
+        st9_P = Parse_Time(1, 0)
     else:
-        st1_B = Parse_Time(9)[1].text.replace(' ', '').replace(',', '').split(';')
+        st1_B = Parse_Time(9, 1)
 
-        st2_in_B = Parse_Time(8)[2].text.replace(' ', '').replace(',', '').split(';')
+        st2_in_B = Parse_Time(8, 2)
 
-        st2_in_P = Parse_Time(8)[3].text.replace(' ', '').replace(',', '').split(';')
+        st2_in_P = Parse_Time(8, 3)
 
-        st3_in_B = Parse_Time(7)[2].text.replace(' ', '').replace(',', '').split(';')
+        st3_in_B = Parse_Time(7, 2)
 
-        st3_in_P = Parse_Time(7)[3].text.replace(' ', '').replace(',', '').split(';')
+        st3_in_P = Parse_Time(7, 3)
 
-        st4_in_B = Parse_Time(6)[2].text.replace(' ', '').replace(',', '').split(';')
+        st4_in_B = Parse_Time(6, 2)
 
-        st4_in_P = Parse_Time(6)[3].text.replace(' ', '').replace(',', '').split(';')
+        st4_in_P = Parse_Time(6, 3)
 
-        st5_in_B = Parse_Time(5)[2].text.replace(' ', '').replace(',', '').split(';')
+        st5_in_B = Parse_Time(5, 2)
 
-        st5_in_P = Parse_Time(5)[3].text.replace(' ', '').replace(',', '').split(';')
+        st5_in_P = Parse_Time(5, 3)
 
-        st6_in_B = Parse_Time(4)[2].text.replace(' ', '').replace(',', '').split(';')
+        st6_in_B = Parse_Time(4, 2)
 
-        st6_in_P = Parse_Time(4)[3].text.replace(' ', '').replace(',', '').split(';')
+        st6_in_P = Parse_Time(4, 3)
 
-        st7_in_B = Parse_Time(3)[2].text.replace(' ', '').replace(',', '').split(';')
+        st7_in_B = Parse_Time(3, 2)
 
-        st7_in_P = Parse_Time(3)[3].text.replace(' ', '').replace(',', '').split(';')
+        st7_in_P = Parse_Time(3, 3)
 
-        st8_in_B = Parse_Time(2)[2].text.replace(' ', '').replace(',', '').split(';')
+        st8_in_B = Parse_Time(2, 2)
 
-        st8_in_P = Parse_Time(2)[3].text.replace(' ', '').replace(',', '').split(';')
+        st8_in_P = Parse_Time(2, 3)
 
-        st9_P = Parse_Time(1)[1].text.replace(' ', '').replace(',', '').split(';')
+        st9_P = Parse_Time(1, 1)
--- parse.py
+++ parse.py
@@ -1,8 +1,9 @@
 import requests as requests
+import re
 from bs4 import BeautifulSoup
 
 
-def Parse_Time(station: int):
+def Parse_Time(station: int, direction: int):
     __IP = 'https://metro-ektb.ru/podrobnyy-grafik-'
 
     station_list = ['0',
@@ -24,4 +25,14 @@ def Parse_Time(station: int):
 
     times = soup.find('div', class_="uss_catalog_content").find_all('li')
 
-    return times
+    split_string = times[direction].text.replace(',', '')
+
+    # split_string.re.split(r'\d{3}', '').split(';')
+
+    text = re.sub(r'\s+|$', '', split_string)
+    text = re.sub(r':\d{4}', '', text)
+
+    return text
+
+
+print(Parse_Time(5, 0))
